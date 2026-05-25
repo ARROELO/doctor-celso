8676a2f782aa4795f002dd02c95872196d2e5991
Arroelo <garciafri@gmail.com>
2026-05-25 09:52:10 +0200

    Switch to @astrojs/netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
 });
